d2e1fd281f58de2cec86620d8c002751410baf39
Jona2808 <jonathan28quina@gmail.com>
2023-04-19 20:49:18 -0500

    actualizo las condiciones de validacion de usuario y contraseña

--- jquinapantaS2/Inicio.xaml.cs
+++ jquinapantaS2/Inicio.xaml.cs
@@ -19,28 +19,28 @@ namespace jquinapantaS2
 
         private void btnIngreso_Clicked(object sender, EventArgs e)
         {
-            const String usuario1 = "2023";
-            const String pass1 = "2023";
+            const String usuario1 = "estudiante2023";
+            const String pass1 = "uisrael2023";
             if ((txtUsuario.Text == usuario1) && (txtContra.Text == pass1))
             {
                 DisplayAlert("UISRAEL", "BIENVENIDO " +usuario1, "Cerrar");
                 Navigation.PushAsync(new Calificaciones(usuario1));
             }
-            if ((txtUsuario.Text != usuario1) && (txtContra.Text == pass1))
+            else if ((txtUsuario.Text != usuario1) && (txtContra.Text == pass1))
             {
                 //desplegar una alerta
                 DisplayAlert("PRECAUCION", "USUARIO INCORRECTO" , "Cerrar");
                 txtUsuario.Text = "";
                 txtContra.Text = "";
             }
-            if ((txtUsuario.Text == usuario1) && (txtContra.Text != pass1))
+            else if ((txtUsuario.Text == usuario1) && (txtContra.Text != pass1))
             {
                 //desplegar una alerta
                 DisplayAlert("PRECAUCION", "CONTRASENA INCORRECTA", "Cerrar");
                 txtUsuario.Text = "";
                 txtContra.Text = "";
             }
-            if ((txtUsuario.Text != usuario1) && (txtContra.Text != pass1))
+            else //((txtUsuario.Text != usuario1) && (txtContra.Text != pass1))
             {
                 //desplegar una alerta
                 DisplayAlert("PRECAUCION", "USUARIO Y CONTRASENA INCORRECTOS", "Cerrar");
